a83d8b74d4af4f3126be24fd4ab200e22cb2b978
programmingwithnavilla <navidbarsalari@gmail.com>
2025-06-27 06:47:51 +0330

    feat: add redis service and module

---
+++ src/cache/redis.service.ts
@@ -0,0 +1,28 @@
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import Redis from 'ioredis';
+
+@Injectable()
+export class RedisService implements OnModuleInit, OnModuleDestroy {
+  private readonly logger = new Logger(RedisService.name);
+  private client: Redis;
+
+  onModuleInit() {
+    this.client = new Redis({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: process.env.REDIS_PORT ? +process.env.REDIS_PORT : 6379,
+    });
+    this.client.on('connect', () => this.logger.log('Redis connected'));
+    this.client.on('error', (err) => this.logger.error('Redis error', err));
+  }
+  getClient(): Redis {
+    return this.client;
+  }
+  async onModuleDestroy() {
+    await this.client.quit();
+  }
+}
