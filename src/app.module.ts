34500f7470c78f678c67de4cf3cf135c02234f14
programmingwithnavilla <navidbarsalari@gmail.com>
2025-06-27 07:20:56 +0330

    use redis module and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RedisModule } from './cache/redis.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [RedisModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
