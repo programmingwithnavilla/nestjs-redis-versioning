b67914aeb349ba2c6c5dc5af3faf81d71962cc38
programmingwithnavilla <navidbarsalari@gmail.com>
2025-06-28 14:37:10 +0330

    delete additional file in update

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -3,8 +3,6 @@ import { z } from 'zod';
 export const UpdateUserSchema = z.object({
   name: z.string().min(2).optional(),
   email: z.string().email().optional(),
-  age: z.number().int().positive().optional(),
-  isActive: z.boolean().optional(),
 });
 
 export type UpdateUserDto = z.infer<typeof UpdateUserSchema>;
